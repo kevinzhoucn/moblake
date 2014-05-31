00994b4736e3ed7dc3723137794f48104699bb0e
zzpf <zzpf2007@gmail.com>
2014-05-31 14:12:40 +0800

    youmi migration

--- db/migrate/20140531035629_create_youmi.rb
+++ db/migrate/20140531035629_create_youmi.rb
@@ -1,9 +1,9 @@
 class CreateYoumis < ActiveRecord::Migration
   def change
     create_table :youmis do |t|
-      t.string :order, :length => 16
-      t.string :app, :length => 16
-      t.string :ad, :length => 50
+      t.string :order, :limit => 16
+      t.string :app, :limit => 16
+      t.string :ad, :limit => 50
       t.integer :adid
       t.string :user
       t.string :device
@@ -11,8 +11,8 @@ class CreateYoumis < ActiveRecord::Migration
       t.float :price
       t.integer :points
       t.integer :time
-      t.string :sig, :length => 8
-      t.string :sign, :length => 36
+      t.string :sig, :limit => 8
+      t.string :sign, :limit => 36
       t.string :source
 
       t.timestamps
